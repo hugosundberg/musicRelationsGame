3a3dd7e692cc54c91023b4543c04ef7e15d75232
Hugo Sundberg <hugosundberg@Hugos-MacBook-Pro.local>
2025-03-05 11:18:02 +0100

    link

--- src/components/StartScreen/StartScreen.tsx
+++ src/components/StartScreen/StartScreen.tsx
@@ -11,7 +11,14 @@ const StartScreen = ({ isVisible, onClick }: StartScreenProps) => {
     return (
       <div className={styles.startScreen}>
         <h2>Update</h2>
-        <p>As of November 2024, Spotify has deprecated the Related Artist API endpoint, effectively causing this app to lose its core functionality. If Spotify decides to re-enable the endpoint, this app will be up and running again! Feel free to visit my <a href="hugosundberg.github.io/hugo">portfolio website</a> to try out my other apps!</p>
+        <p>
+          As of November 2024, Spotify has deprecated the Related Artist API
+          endpoint, effectively causing this app to lose its core functionality.
+          If Spotify decides to re-enable the endpoint, this app will be up and
+          running again! Feel free to visit my{" "}
+          <a href="/hugo">portfolio website</a> to try out
+          my other apps!
+        </p>
         <h2>Welcome!</h2>
         <p>
           When the game starts you will be prompted with a selection of artists.
